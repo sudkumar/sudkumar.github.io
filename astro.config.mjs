38313f8d29e48ec1a9dc479b29cd8890a559b797
Sudhir Mitharwal <luckysud4@gmail.com>
2024-04-20 19:23:04 +0530

    fix(post): add first post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), tailwind(), sitemap()]
+});
